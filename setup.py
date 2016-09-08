ef797d907c51a4c5093705a61de00025fc0d1914
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-09-08 16:30:15 +0200

    Use README for long_description

--- setup.py
+++ setup.py
@@ -18,10 +18,7 @@ if __name__ == '__main__':
       name = 'prance',
       version = '0.1.1',
       description = 'Swagger/OpenAPI 2.0 Parser',
-      long_description = """\
-Prances provices parsers for Swagger/OpenAPI 2.0 API specifications in Python.
-It uses `swagger_spec_validator` to validate specifications, but additionally
-resolves JSON references in accordance with the Swagger spec.""",
+      long_description = open('README.rst').read(),
       # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers = [
         'Development Status :: 4 - Beta',
