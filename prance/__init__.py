0796fc8e6d349eb5e475f5146cc4dbabeb0cbedb
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-10-25 10:10:03 +0200

    Bump version: 0.2.0 → 0.2.1

--- prance/__init__.py
+++ prance/__init__.py
@@ -12,7 +12,7 @@ __author__ = 'Jens Finkhaeuser'
 __copyright__ = 'Copyright (c) 2016 Jens Finkhaeuser'
 __license__ = 'MIT +no-false-attribs'
 __all__ = ('util', 'cli')
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 
 # Just re-use the error, but hide the namespace
