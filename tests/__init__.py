0d20503d1242ee100f4a22af06bbc92bd24606fc
Jens Finkhaeuser <jens@finkhaeuser.de>
2017-05-02 11:00:28 +0200

    Fix typo

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Test suite module for Sashay."""
+"""Test suite module for prance."""
 
 __author__ = 'Jens Finkhaeuser'
 __copyright__ = 'Copyright (c) 2016 Jens Finkhaeuser'
