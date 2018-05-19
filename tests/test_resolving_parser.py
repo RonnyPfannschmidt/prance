ee0b3de79b474bf66ad4cdc7a4b632c9da498fbf
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-05-19 11:56:13 +0200

    Bump copyright notice

--- tests/test_resolving_parser.py
+++ tests/test_resolving_parser.py
@@ -2,7 +2,7 @@
 """Test suite for prance.ResolvingParser ."""
 
 __author__ = 'Jens Finkhaeuser'
-__copyright__ = 'Copyright (c) 2016-2017 Jens Finkhaeuser'
+__copyright__ = 'Copyright (c) 2016-2018 Jens Finkhaeuser'
 __license__ = 'MIT +no-false-attribs'
 __all__ = ()
 
