0603ea67dc8c25750a2cdb8310ca4a265c72b13b
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-05-22 18:12:24 +0200

    Implement OpenAPI Schema Object validation

--- prance/data.py
+++ prance/data.py
@@ -7,7 +7,7 @@ __license__ = 'MIT +no-false-attribs'
 __all__ = ()
 
 
-def validate_schema(data, schema):
+def validate_json_schema(data, schema, subschema = None):
   """
   Validate data against a JSON schema.
 
@@ -16,20 +16,30 @@ def validate_schema(data, schema):
 
   :param mixed data: The data to validate.
   :param mixed schema: The schema to validate against.
+  :param mixed subschema: [optional] If provided, the specific subschema
+      of the schema to validate against. In this case, the data is validated
+      against the subschema, but the entire schema is used for e.g. reference
+      resolution.
   :return: True if validation is successful.
   :rtype: bool
   :raises: prance.util.exceptions.SchemaError if the schema is invalid.
   :raises: prance.util.exceptions.ValidationError if the data is invalid.
   """
-  # Perform minimal validation of the schema itself
-
   # Perform validation with jsonschema
-  from jsonschema import validate
+  from jsonschema.validators import validator_for
   from jsonschema.exceptions import ValidationError as JSEValidationError
   from jsonschema.exceptions import SchemaError as JSESchemaError
 
   try:
-    validate(data, schema)
+    # Get validator
+    validator = validator_for(schema)
+
+    # Validate schema itself
+    validator.check_schema(schema)
+
+    # Now validate with the subschema - if it's None, validate() will use the
+    # entire schema, exactly like we want.
+    validator(schema).validate(data, _schema = subschema)
   except JSEValidationError as exc:
     from .util.exceptions import raise_from, ValidationError
     raise_from(ValidationError, exc)
@@ -39,6 +49,42 @@ def validate_schema(data, schema):
 
   # Just in case the result is tested
   return True
+
+
+def validate_schema_object(data, schema):
+  """
+  Validate data against a JSON schema and the OpenAPI specs.
+
+  The OpenAPI specs are a little more restrictive when it comes
+  to Schema Objects than JSON Schema is. We therefore first
+  validate the schema against the OpenAPI specs, before validating
+  the data object.
+
+  :param mixed data: The data to validate.
+  :param mixed schema: The schema to validate against.
+  :return: True if validation is successful.
+  :rtype: bool
+  :raises: prance.util.exceptions.SchemaError if the schema is invalid.
+  :raises: prance.util.exceptions.ValidationError if the data is invalid.
+  """
+  from .util import validation_backends
+  if 'openapi-spec-validator' not in validation_backends():
+    raise ImportError('The "openapi-spec-validator" backend is required for '
+        'data validation, as only OpenAPI 3 specs are permitted here.')
+
+  # First, ensure that the schema is valid according to the OpenAPI
+  # specs.
+  from .util.exceptions import ValidationError
+  try:
+    from openapi_spec_validator import schema_v3
+    validate_json_schema(schema, schema_v3,
+        subschema = schema_v3['definitions']['schemaOrReference'])
+  except ValidationError as exc:
+    from .util.exceptions import raise_from, SchemaError
+    raise_from(SchemaError, exc)
+
+  # Next, validate data according to the sub schema
+  return validate_json_schema(data, schema)
 
 
 def validate_specs(data, specs, path = None):
@@ -59,4 +105,4 @@ def validate_specs(data, specs, path = None):
   schema = path_get(specs, path)
 
   # Then validate with that schema.
-  return validate_schema(data, schema)
+  return validate_json_schema(data, schema)
--- tests/test_data.py
+++ tests/test_data.py
@@ -16,7 +16,7 @@ def petstore_parser():
   return ResolvingParser('tests/petstore.yaml')
 
 
-def test_validate_schema():
+def test_validate_json_schema():
   # Taken directly from jsonschema documentation
   schema = {
     "type" : "object",
@@ -27,17 +27,59 @@ def test_validate_schema():
   }
 
   # If no exception is raised by validate(), the instance is valid.
-  res = data.validate_schema({"name" : "Eggs", "price" : 34.99}, schema)
+  res = data.validate_json_schema({"name" : "Eggs", "price" : 34.99}, schema)
   assert res is True
 
   from prance.util.exceptions import ValidationError
   with pytest.raises(ValidationError):
-    data.validate_schema({"name" : "Eggs", "price" : "Invalid"}, schema)
+    data.validate_json_schema({"name" : "Eggs", "price" : "Invalid"}, schema)
 
   # Try a bad schema
   from prance.util.exceptions import SchemaError
   with pytest.raises(SchemaError):
-    data.validate_schema({'foo': 123}, {'type': 'does-not-exist'})
+    data.validate_json_schema({'foo': 123}, {'type': 'does-not-exist'})
+  with pytest.raises(SchemaError):
+    data.validate_json_schema({'foo': 123}, {'type': 'integer', 'multipleOf': 0})
+
+  # JSON schema allows multiple types - OpenAPI does not!
+  schema['type'] = ['object', 'integer']
+  res = data.validate_json_schema({"name" : "Eggs", "price" : 34.99}, schema)
+  assert res is True
+
+  res = data.validate_json_schema(42, schema)
+  assert res is True
+
+
+def test_validate_schema_object():
+  # Taken directly from jsonschema documentation
+  schema = {
+    "type" : "object",
+    "properties" : {
+      "price" : {"type" : "number"},
+      "name" : {"type" : "string"},
+    },
+  }
+
+  # If no exception is raised by validate(), the instance is valid.
+  res = data.validate_schema_object({"name" : "Eggs", "price" : 34.99}, schema)
+  assert res is True
+
+  from prance.util.exceptions import ValidationError
+  with pytest.raises(ValidationError):
+    data.validate_schema_object({"name" : "Eggs", "price" : "Invalid"}, schema)
+
+  # Try a bad schema
+  from prance.util.exceptions import SchemaError
+  with pytest.raises(SchemaError):
+    data.validate_schema_object({'foo': 123}, {'type': 'does-not-exist'})
+
+  # JSON schema allows multiple types - OpenAPI does not!
+  schema['type'] = ['object', 'integer']
+  with pytest.raises(SchemaError):
+    data.validate_schema_object({"name" : "Eggs", "price" : 34.99}, schema)
+  with pytest.raises(SchemaError):
+    data.validate_schema_object(42, schema)
+
 
 
 def test_validate_specs(petstore_parser):
