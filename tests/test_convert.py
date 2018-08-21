3969ff2efca4640bffd08f4210cd07d66be13d34
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-08-21 10:19:10 +0200

    Mark tests as xfail that depend on external URLs

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -21,6 +21,7 @@ def petstore_json():
   return fs.read_file('tests/OpenAPI-Specification/examples/v2.0/json/petstore.json')
 
 
+@pytest.mark.xfail
 def test_convert_petstore_yaml(petstore_yaml):
   converted, content_type = convert.convert_str(petstore_yaml)
 
@@ -36,6 +37,7 @@ def test_convert_petstore_yaml(petstore_yaml):
   assert parsed['openapi'].startswith('3.')
 
 
+@pytest.mark.xfail
 def test_convert_petstore_json(petstore_json):
   converted, content_type = convert.convert_str(petstore_json)
 
@@ -51,6 +53,7 @@ def test_convert_petstore_json(petstore_json):
   assert parsed['openapi'].startswith('3.')
 
 
+@pytest.mark.xfail
 def test_convert_petstore_yaml_explicit_name(petstore_yaml):
   converted, content_type = convert.convert_str(petstore_yaml, filename = 'foo.yml')
 
@@ -58,6 +61,7 @@ def test_convert_petstore_yaml_explicit_name(petstore_yaml):
   assert 'yaml' in content_type
 
 
+@pytest.mark.xfail
 def test_convert_url():
   from prance.util import url
   converted, content_type = convert.convert_url(url.absurl('python://tests/specs/petstore.yaml'))
@@ -74,6 +78,7 @@ def test_convert_url():
   assert parsed['openapi'].startswith('3.')
 
 
+@pytest.mark.xfail
 def test_convert_spec():
   from prance import BaseParser, ResolvingParser, ValidationError
   parser = BaseParser('tests/specs/petstore.yaml')
@@ -91,6 +96,7 @@ def test_convert_spec():
   assert isinstance(converted, ResolvingParser)
 
 
+@pytest.mark.xfail
 def test_convert_parser_lazy_swagger_backend():
   from prance import BaseParser, ResolvingParser, ValidationError
   parser = BaseParser('tests/specs/petstore.yaml')
@@ -110,6 +116,7 @@ def test_convert_parser_lazy_swagger_backend():
 
 from prance.util import validation_backends
 if 'openapi-spec-validator' in validation_backends():
+  @pytest.mark.xfail
   def test_convert_parser_validated():
     from prance import BaseParser
     parser = BaseParser('tests/specs/petstore.yaml', backend = 'openapi-spec-validator')
