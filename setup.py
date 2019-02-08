9a09b6dc7042e2408f9506989f1f6cc98790279a
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-02-08 12:21:56 +0100

    Should support Python 3.7, really.

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
       ],
