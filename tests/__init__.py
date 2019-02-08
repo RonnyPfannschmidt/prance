7556d39feab876ab455640e1d388be5757f06f2b
Jens Finkhaeuser <jens@finkhaeuser.de>
2019-02-08 10:17:08 +0100

    Add decorator making tests dependent on installed modules

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,25 @@ __author__ = 'Jens Finkhaeuser'
 __copyright__ = 'Copyright (c) 2016-2018 Jens Finkhaeuser'
 __license__ = 'MIT +no-false-attribs'
 __all__ = ()
+
+def run_if_present(name):
+  import importlib
+  exists = False
+  try:
+    importlib.import_module(name)
+    exists = True
+  except ImportError:
+    pass
+
+  print('exists', name, exists)
+
+  def wrapper(func):
+    import functools
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+      if exists:
+        return func(*args, **kwargs)
+      import pytest
+      pytest.skip('Required dependency "%s" does not exist.' % (name,))
+    return wrapped
+  return wrapper
